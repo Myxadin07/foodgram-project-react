3e00e6657a18a659d6fe0c9c881b8b58eb1beea9
Myxadin07 <myxadin1994@gmail.com>
2023-07-29 22:21:36 +0300

    да

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -4,13 +4,12 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
-from rest_framework import status, exceptions
+from rest_framework import status, exceptions, mixins, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import (
     IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
 )
 from rest_framework.response import Response
-from rest_framework import mixins, viewsets
 
 from recipes.models import (
     Ingredients, IngredientsInRecipes, Recipes, ShoppingCart, Tags
