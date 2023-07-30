45153b70f8e5b730d3e28b387b20028102a7d42e
Myxadin07 <myxadin1994@gmail.com>
2023-07-30 17:35:09 +0300

    да

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -166,7 +166,7 @@ class RecipesViewSet(viewsets.ModelViewSet):
     )
     def shopping_cart(self, request, pk):
         if request.method == 'POST':
-            return self.add_to(ShoppingCart, request.user, pk)
+            return self.add_to(ShoppingCart, request.user.id, pk)
         elif request.method == 'DELETE':
             return self.delete_from(ShoppingCart, request.user.id, pk)
 
